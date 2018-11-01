edf30811c77302017cd7120d26999e5967a9cbe1
Tadej Slamic <tadej.slamic@gmail.com>
2018-11-01 09:19:41 +0100

    Adding Android-specific code.

--- app/src/main/java/me/tadej/multiplatformexample/MainActivity.kt
+++ app/src/main/java/me/tadej/multiplatformexample/MainActivity.kt
@@ -1,12 +1,14 @@
 package me.tadej.multiplatformexample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import greet
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        greeter.text = greet()
     }
 }
